5857c7dd59373247923999e7bfa1fec3ec303d03
Sun Seng David TAN <sutan@redhat.com>
2020-08-12 15:36:36 +0200

    fixing webrtc issues
    Signed-off-by: Sun Seng David TAN <sutan@redhat.com>

--- src/extension.ts
+++ src/extension.ts
@@ -8,8 +8,7 @@ import PortalBinding from './PortalBinding';
 const fetch = require('node-fetch');
 const constants = require('./constants');
 const globalAny: any = global;
-const wrtc = require('electron-webrtc-patched')();
-// const wrtc = require('electron-webrtc')();
+const wrtc = require('wrtc');
 
 globalAny.window = {};
 globalAny.window = global;
